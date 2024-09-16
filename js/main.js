3bd91eb51d1d2a55dd1371ab81418a3d8aaa3184
breaking16 <w.w804.162@gmail.com>
2024-09-16 20:58:37 +0300

    4half 3party

--- js/main.js
+++ js/main.js
@@ -9,9 +9,26 @@ const swiper = new Swiper(".slider-main-block", {
 });
 
 //Таби
+
+const tabNavItems = document.querySelectorAll(".tabs-deals__button");
+const tabItems = document.querySelectorAll(".item-tabs");
 document.addEventListener("click", function (e) {
   const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
   if (targetElement.closest(".tabs-deals__button")) {
-    console.log("yes");
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+      // tabNavItem.classList.remove("active");
+    });
+    targetElement.classList.add("active");
+    tabItems[currentActiveIndex].classList.remove("active");
+    tabItems[newActiveIndex].classList.add("active");
   }
 });
